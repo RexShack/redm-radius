9207d8c5d2cb81d291853170fb2e1e2c0137f68f
RexshackGaming <rexshackuk@gmail.com>
2023-09-05 11:06:39 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.maxCulling = 350.0
+Config.minCulling = 100.0
+Config.stepCulling = 80.0
+Config.numberPlayersAround = 28
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'redm-radius'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts {
+	'client/client.lua'
+}
+
+server_scripts {
+	'server/server.lua'
+}
+
+lua54 'yes'
